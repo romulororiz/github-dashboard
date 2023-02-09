b41fa38c3bc9e7d63837f87632d48fcad2472362
Romulo Roriz <romulonmroriz@gmail.com>
2023-02-09 17:48:40 +0100

    Setup Providers

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@styles/scss/index.scss';
 import { GithubProvider } from '@context/github/GithubContext';
+import { AlertProvider } from '@context/alert/AlertContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<GithubProvider>
-			<App />
+			<AlertProvider>
+				<App />
+			</AlertProvider>
 		</GithubProvider>
 	</React.StrictMode>
 );
